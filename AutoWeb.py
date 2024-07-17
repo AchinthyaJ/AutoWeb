3085dd6a57eda746e5910bc30bc5964fb0fd0569
Inchara Jayaram <incharajayaram2020@gmail.com>
2024-07-17 20:19:55 +0530

    Fix Errors 2

--- AutoWeb.py
+++ AutoWeb.py
@@ -161,7 +161,7 @@ class WebsiteGenerator:
         dropdown.grid(row=row, column=1, sticky="ew", padx=5, pady=5)
         dropdown.set(options[0])
         setattr(self, f"{label.lower().replace(':', '')}_var", var)
-        setattr(self, f"{label.lower().replace(':', '')}_dropdown", dropdown)
+        setattr(self, f"{label.lower().replace(' ', '_').replace(':', '')}_dropdown", dropdown)
 
     def add_custom_section(self):
         section_window = tk.Toplevel(self.master)
@@ -207,16 +207,18 @@ class WebsiteGenerator:
         font_choice = self.font_dropdown.current() + 1
         layout_choice = self.layout_dropdown.current() + 1
         bootstrap_choice = self.add_bootstrap_dropdown.current() + 1
-       
+        
+        
             
 
         if bootstrap_choice == 1:
             bootstrap_entry = """<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">"""
             navbar = self.add_navbar_dropdown.current()+ 1
             if navbar == 1:
-                navbar_entry = """<nav class="navbar navbar-expand-lg bg-body-tertiary">
+                navbar_entry = """
+                <nav class="navbar navbar-expand-lg bg-body-tertiary" style="margin-top: 0px">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">{header}</a>
+    <a class="navbar-brand" href="#">"""+ header +"""</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -235,10 +237,10 @@ class WebsiteGenerator:
                 {"".join(f'<li><a href="#{section["id"]}">{section["title"]}</a></li>' for section in custom_sections)}
                  </li>
                  <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a class="nav-link dropdown-toggle" href="#ul" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   More
                 </a>
-                 <ul class="dropdown-menu">
+                 <ul class="dropdown-menu" id="ul">
                  <li><a class="dropdown-item" href="#">Login</a></li>
                  <li><a class="dropdown-item" href="#">Sign Up</a></li>
                  <li><hr class="dropdown-divider"></li>
@@ -255,7 +257,9 @@ class WebsiteGenerator:
                   </form>
                </div>
               </div>
-             </nav>""" 
+             </nav>
+             <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>""" 
             else:
                 navbar_entry="""<nav>
             <ul>
@@ -266,7 +270,7 @@ class WebsiteGenerator:
                 {"".join(f'<li><a href="#{section["id"]}">{section["title"]}</a></li>' for section in custom_sections)}
             </ul>
         </nav>
-        <h1>{header}</h1>"""
+        <h1>"""+header+"""</h1>"""
  
         # Define color schemes
         # Define color schemes
